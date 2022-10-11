f3db1d915a342ec9a867eb936039c319011ee587
Michael <mmoses1127@gmail.com>
2022-10-11 13:41:36 -0700

    resize flames

--- src/flame.js
+++ src/flame.js
@@ -5,11 +5,11 @@ export default class Flame extends MovingObject {
     constructor(pos, game) {
         super({
             color: 'orange', 
-            radius: 50, 
+            radius: 130, 
             vel: [0,0],
             pos: pos, 
             game: game
         });
-        this.image = '../assets/imagery/flame_2.png';
+        this.image = '../assets/imagery/flame_ball.gif';
     }
 }
